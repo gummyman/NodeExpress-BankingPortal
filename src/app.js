6a86b4db8f056c77bcf13e3b6c51d88d96188a04
gummyman <gazisakib1995@gmail.com>
2020-08-28 17:01:38 -0400

    Use Express to Create a Server

--- src/app.js
+++ src/app.js
@@ -4,10 +4,11 @@ const express = require('express');
 
 const app = express();
 
-app.set('views',path.join(__dirname,'/src/views'));
+app.set('views',path.join(__dirname, '/views'));
 app.set('view engine','ejs');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 app.get('/',
     (req, res)=>{
