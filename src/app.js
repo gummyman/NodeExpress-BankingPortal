ea58325d5f2537308d9738c2a2b10fa7217c8fb5
gummyman <gazisakib1995@gmail.com>
2020-08-28 18:38:11 -0400

    Handling Form Data Commit 1

--- src/app.js
+++ src/app.js
@@ -8,12 +8,15 @@ app.set('views',path.join(__dirname, '/views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({extender:true}));
 
 const accountData = fs.readFileSync('src/json/accounts.json',{encoding:'utf8'});
 const accounts = JSON.parse(accountData);
 
 const userData = fs.readFileSync('src/json/users.json',{encoding:'utf8'});
 const users = JSON.parse(userData);
+
+
 
 app.get('/',
     (req, res)=>{
@@ -42,6 +45,50 @@ app.get('/credit',
 app.get('/profile',
     (req,res)=>{
         res.render('profile', {user:users[0]});
+    }
+)
+
+app.get('/payment',
+    (req,res)=>{
+        res.render('payment',{account: accounts.credit});
+    }
+)
+
+app.post('/payment',
+    (req,res)=>{
+        const amount = parseInt(req.body.amount);
+        accounts.credit.balance -= amount;
+        accounts.credit.available += amount;
+
+        const accountsJSON = JSON.stringify(accounts);
+
+        fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,{encoding:'utf8'});
+
+        res.render('payment', {message : 'Payment Successful', account: accounts.credit});
+    }
+)
+
+app.get('/transfer',
+    (req,res)=>{
+        res.render('transfer');
+    }
+);
+
+app.post('/transfer',
+    (req,res)=>{
+        const from = req.body.from;
+        const to = req.body.to;
+        const amount = parseInt(req.body.amount);
+
+        accounts[from].balance = parseInt(accounts[from].balance)-amount;
+        accounts[to].balance = parseInt(accounts[to].balance)+amount;
+
+        const accountsJSON = JSON.stringify(accounts);
+
+        fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,{encoding:'utf8'});
+        fs.writeFileSync(path.join(__dirname,'json/accounts_backup.json'),accountData,{encoding:'utf8'});
+
+        res.render('transfer',{message:'Transfer Completed'});
     }
 )
 
